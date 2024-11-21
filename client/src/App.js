e9deffc00698d59b4263554d0b85cf20a3a66f08
Louis Klein <louisklein71@gmail.com>
2024-11-21 02:20:41 -0800

    api url

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 // import components
 import Home from './components/home.js';
 
+const API_URL = 'https://pokeapi.co/api/v2/';
+
 function App() {
   return (
     <div className="App">
