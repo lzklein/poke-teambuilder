b6146571cad06e18a24601b4302b798831da7789
Louis Klein <louisklein71@gmail.com>
2024-11-24 02:54:26 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,20 @@ import React, { useState, useEffect } from 'react';
 // import components
 import Home from './components/home.js';
 
-const API_URL = 'https://pokeapi.co/api/v2/';
+const API_BASE_URL = 'https://pokeapi.co/api/v2/';
 
 function App() {
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/pokemon?limit=10000&offset=0`)
+      .then(r => r.json())
+      .then(data => setPokemon(data.results)) // Correctly handle the data structure
+      .catch(err => console.error("Error fetching Pokemon data:", err)); // Handle errors
+  }, []);
+
+  const [pokemon, setPokemon] = useState([]);
+
+  pokemon.map((mon)=>{console.log(mon.name)});
+  
   return (
     <div className="App">
       <Home/>
