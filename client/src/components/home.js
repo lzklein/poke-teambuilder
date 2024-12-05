431504b8f0d15352c79e3d543e508a95051fe88b
Louis Klein <louisklein71@gmail.com>
2024-12-04 16:31:55 -0800

    Update home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Team from './team.js';
-
+import Overview from './overview.js';
 const home = ({pokemon}) => {
   return (
     <div>
         <Team pokemon={pokemon}/>
+        <Overview/>
     </div>
   )
 }
