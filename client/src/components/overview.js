f7cbd4f8f60d27abd06b54d94eb676634308fede
Louis Klein <louisklein71@gmail.com>
2024-12-04 16:31:40 -0800

    Create overview.js

---
+++ client/src/components/overview.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Stats from './stats';
+import Coverage from './coverage';
+import Weakness from './weakness';
+
+const overview = () => {
+  return (
+    <div>
+      <Stats/>
+    </div>
+  )
+}
+
+export default overview
