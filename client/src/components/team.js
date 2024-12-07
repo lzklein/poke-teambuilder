f5fe40ef63245be3df7d9553d6226e4d2712cee3
Louis Klein <louisklein71@gmail.com>
2024-12-07 00:53:24 -0800

    Update team.js

--- client/src/components/team.js
+++ client/src/components/team.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import Pokecard from './pokecard.js';
 
-const team = ({pokemon}) => {
+const team = ({pokemon, url}) => {
   return (
-    <div>
-        {pokemon.slice(0, 6).map((name, index) => (
-        <Pokecard key={index} name={name} />
+    <div className="team-container">
+        {pokemon.slice(0, 6).map((index) => (
+        <Pokecard key={index} pokemon={pokemon} url={url}/>
         ))}
     </div>
   )
