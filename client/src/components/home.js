c982f5c360d43885c0d0b33f43f4641fe23bb29b
Louis Klein <louisklein71@gmail.com>
2024-12-07 00:53:29 -0800

    Update home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
-import Team from './team.js';
-import Overview from './overview.js';
-const home = ({pokemon}) => {
+import Header from './header';
+import Team from './team';
+import Checklist from './checklist';
+import Overview from './overview';
+import Footer from './footer';
+
+const home = ({pokemon, url}) => {
   return (
-    <div>
-        <Team pokemon={pokemon}/>
-        <Overview/>
+    <div className="layout">
+      <Header />
+      <div className="columns">
+        <Team pokemon={pokemon} url={url}/>
+        <div className="right-column">
+          <Checklist />
+          <Overview />
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default home
