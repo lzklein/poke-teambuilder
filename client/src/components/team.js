dc926dc9008f2de45cb5e7e54abca522ebf5e1ce
Louis Klein <louisklein71@gmail.com>
2024-12-11 10:24:40 -0800

    Update team.js

--- client/src/components/team.js
+++ client/src/components/team.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import Pokecard from './pokecard.js';
 
-const Team = ({pokemon, url, setTeamData, setTeamStats}) => {
+const Team = ({pokemon, url, setTeamData, setTeamStats, setTypeCounts}) => {
   return (
     <div className="team-container">
         {pokemon.slice(0, 6).map((mon, index) => (
-        <Pokecard key={index} pokemon={pokemon} url={url} setTeamData={setTeamData} ind={index} setTeamStats={setTeamStats}/>
+        <Pokecard key={index} pokemon={pokemon} url={url} setTeamData={setTeamData} ind={index} setTeamStats={setTeamStats} setTypeCounts={setTypeCounts}/>
         ))}
     </div>
   )
