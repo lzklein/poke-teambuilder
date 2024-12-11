129700a1b3622a2f8a55f24fde68c82ee52e3961
Louis Klein <louisklein71@gmail.com>
2024-12-11 10:07:01 -0800

    Update stats.js

--- client/src/components/stats.js
+++ client/src/components/stats.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import RadarChart from './radarchart.js';
 
-const stats = () => {
+const stats = ({teamStats}) => {
   return (
     <div style={{maxWidth:'300px'}}>
       <RadarChart/>
