c662d0076be1475dec36798c010289b23b1448f1
Louis Klein <louisklein71@gmail.com>
2024-12-11 07:18:06 -0800

    type tracker

--- client/src/components/coverage.js
+++ client/src/components/coverage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const coverage = () => {
+const Coverage = () => {
+  const types = [
+    "normal", "fire", "water", "electric", "grass", "ice", 
+    "fighting", "poison", "ground", "flying", "psychic", "bug", 
+    "rock", "ghost", "dragon", "dark", "steel", "fairy"
+  ];
+
   return (
-    <div>coverage</div>
-  )
-}
+    <div>
+      <h4 className="chartLabel">Coverage</h4>
+      <div className="coverage-container">
+        <div className="coverage-grid">
+          {types.map((type) => (
+            <div className="coverage-item" key={type}>
+              <div className={`type-box ${type}`}>
+                {type}
+              </div>
+              <div className="coverage-count">0</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default coverage
+export default Coverage;
--- client/src/components/weakness.js
+++ client/src/components/weakness.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
 const weakness = () => {
+  const types = [
+    "normal", "fire", "water", "electric", "grass", "ice", 
+    "fighting", "poison", "ground", "flying", "psychic", "bug", 
+    "rock", "ghost", "dragon", "dark", "steel", "fairy"
+  ];
+
   return (
-    <div>weakness</div>
-  )
-}
+    <div>
+      <h4 className="chartLabel">Weakness</h4>
+      <div className="coverage-container">
+        <div className="coverage-grid">
+          {types.map((type) => (
+            <div className="coverage-item" key={type}>
+              <div className={`type-box ${type}`}>
+                {type}
+              </div>
+              <div className="coverage-count">0</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+  );
+};
 
 export default weakness
