9b9d90df7da40f616fe435b7b5eb6fa41dbb390d
Louis Klein <louisklein71@gmail.com>
2024-12-12 00:48:06 -0800

    changed type storage format

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -9,27 +9,8 @@ import Footer from './footer';
 const Home = ({pokemon, url}) => {
   const [teamData, setTeamData] = useState([[],[],[],[],[],[]]);
   const [teamStats, setTeamStats] = useState([[],[],[],[],[],[]]);
-  const [typeCounts, setTypeCounts] = useState({
-    normal: 0,
-    fire: 0,
-    water: 0,
-    grass: 0,
-    electric: 0,
-    ice: 0,
-    fighting: 0,
-    poison: 0,
-    ground: 0,
-    flying: 0,
-    psychic: 0,
-    bug: 0,
-    rock: 0,
-    ghost: 0,
-    dark: 0,
-    dragon: 0,
-    steel: 0,
-    fairy: 0,
-  });
-
+  const [typeCounts, setTypeCounts] = useState(['','','','','','']);
+  console.log(typeCounts)
   
   return (
     <div className="layout">
