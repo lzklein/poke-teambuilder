b1139e361dd572732598f42a77caa78daee0278e
Louis Klein <louisklein71@gmail.com>
2024-12-14 01:26:58 -0800

    move type tracker

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -10,16 +10,43 @@ const Home = ({pokemon, url}) => {
   const [teamData, setTeamData] = useState([[],[],[],[],[],[]]);
   const [teamStats, setTeamStats] = useState([[],[],[],[],[],[]]);
   const [typeCounts, setTypeCounts] = useState(['','','','','','']);
-  console.log(typeCounts)
+  const [moveTypes, setMoveTypes] = useState({
+    normal: 0,
+    fire: 0,
+    water: 0,
+    grass: 0,
+    electric: 0,
+    ice: 0,
+    fighting: 0,
+    poison: 0,
+    ground: 0,
+    flying: 0,
+    psychic: 0,
+    bug: 0,
+    rock: 0,
+    ghost: 0,
+    dark: 0,
+    dragon: 0,
+    steel: 0,
+    fairy: 0,
+  })
+
+  console.log(moveTypes)
   
   return (
     <div className="layout">
       <Header />
       <div className="columns">
-        <Team pokemon={pokemon} url={url} setTeamData={setTeamData} setTeamStats={setTeamStats} setTypeCounts={setTypeCounts}/>
+        <Team pokemon={pokemon}
+        url={url} 
+        setTeamData={setTeamData} 
+        setTeamStats={setTeamStats} 
+        setTypeCounts={setTypeCounts}
+        setMoveTypes={setMoveTypes}
+        />
         <div className="right-column">
           <Checklist teamData={teamData}/>
-          <Overview teamStats={teamStats} types={typeCounts}/>
+          <Overview teamStats={teamStats} typeCounts={typeCounts} moveTypes={moveTypes}/>
         </div>
       </div>
       <Footer />  
