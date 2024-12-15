882fbf8397ddaaceb7eab555ad7592e0e50397e8
Louis Klein <louisklein71@gmail.com>
2024-12-15 02:10:47 -0800

    title

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div>header</div>
+    <div>
+        <h2 style={{marginBottom:'-25px'}}>
+            Pokemon Team Builder
+        </h2>
+    </div>
   )
 }
 
