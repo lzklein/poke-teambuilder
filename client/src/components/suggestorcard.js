367fba2c44a174a789562f547d4082f084f4caf7
Louis Klein <louisklein71@gmail.com>
2024-12-18 04:36:18 -0800

    render requirement

--- client/src/components/suggestorcard.js
+++ client/src/components/suggestorcard.js
@@ -1,38 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import Missingno from '../assets/missingno.png';
 
-const SuggestorCard = ({name, url}) => {
-    const [cardData, setCardData] = useState({});
-    useEffect(() => {
+const SuggestorCard = ({ name, url, renderDisplay }) => {
+  const [cardData, setCardData] = useState(null);
+
+  useEffect(() => {
+    if(renderDisplay){
         fetch(`${url}pokemon/${name.toLowerCase()}`)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error('Failed to fetch Pokémon data');
-            }
-            return response.json();
-          })
-          .then((data) => {
-            setCardData(data);
-          })
-          .catch((error) => console.error('Error fetching Pokémon data:', error));
-      }, [url, name]);
-    
-      console.log(cardData);
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch Pokémon data');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setCardData(data);
+        })
+        .catch((error) => console.error('Error fetching Pokémon data:', error));
+    }
+  }, [url, name, renderDisplay]);
 
   return (
     <div className="pokecardContainer">
-        <img
-              src={
-                cardData
-                  ? cardData.sprites.front_default
-                  : Missingno
-              }
-              alt="Pokémon"
-              className="pokemonImage"
-        />
-        <h3>{cardData.name.charAt(0).toUpperCase() + cardData.name.slice(1)}</h3>
+      <img
+        src={cardData && cardData.sprites ? cardData.sprites.front_default : Missingno}
+        alt={name || 'Pokémon'}
+        className="pokemonImage"
+      />
+      <h3>
+        {cardData && cardData.name
+          ? cardData.name.charAt(0).toUpperCase() + cardData.name.slice(1)
+          : 'Unknown Pokémon'}
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default SuggestorCard
+export default SuggestorCard;
